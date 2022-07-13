f3f26b1cb553f9c27d9d13ad145946ab9c11544f
raashith <raashithmohammed305@gmail.com>
2022-07-13 13:33:42 -0800

    New program

---
+++ print_even.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+	int i;
+	printf("Enter the even no between 50:");
+	for(i = 1; i <= 50; i++)
+{
+			
+	 if (i%2==0)
+				
+	{	
+		printf("%d\n",i);
+	
+		}
+		
+}
+ 
+	 
+	
+	return 0;
+}
