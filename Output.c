3213b19f8385ebc4f5bebd279ae3c39c5ce6ce2e
raashith <raashithmohammed305@gmail.com>
2022-07-14 12:29:00 -0800

    New program

---
+++ Output.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+	printf("=====Resulte=====");
+	printf("\n\n");
+	printf("Regno :22503545");
+	printf("\n\n");
+	printf("Name:raashith");
+	printf("\n\n\n");
+	printf("*************************");
+	printf("\n\n");
+	printf("subs\t Mark\t Result");
+	printf("\n\n");
+	printf("*************************");
+	printf("\n\n");
+	printf("sub1:\t 50\t Pass");
+	printf("\n\n");
+	printf("**************************");
+	printf("\n\n");
+	printf("sub2:\t 55\t Pass");
+	printf("\n\n");
+	printf("**************************");
+	printf("\n\n");
+	printf("sub3:\t 60\t Pass");
+	printf("\n\n");
+	printf("**************************");
+	return 0;
+}
